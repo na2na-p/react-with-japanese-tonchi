0717ebcf4e24d19cad8b60e1407e9529baea374a
na2na-p <na2na@na2na.dev>
2022-10-20 21:55:34 +0900

    Add ChildToParent

---
+++ src/examples/ChildToParent/Child.tsx
@@ -0,0 +1,26 @@
+import React, {Dispatch, SetStateAction, ChangeEvent} from 'react';
+
+import {TextField} from '@mui/material';
+
+type ChildProps = {
+	setName: Dispatch<SetStateAction<string>>
+}
+
+export default ({setName}: ChildProps) => {
+	const textFieldOnChange = (
+		e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+	) => {
+		setName(e.target.value);
+	};
+
+	return (
+		<>
+			<TextField
+				label="名前"
+				variant="outlined"
+				size="small"
+				onChange={textFieldOnChange}
+			/>
+		</>
+	);
+};
---
+++ src/examples/ChildToParent/index.tsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+import Divider from '@components/Divider';
+
+import Child from './Child';
+
+export default () => {
+	const [name, setName] = useState('');
+
+	return <>
+		<div>子から親に(見かけ上は)値が渡るやつ</div>
+		<Divider />
+		<Child setName={setName} />
+		<Divider />
+		<div>名前: {name}</div>
+	</>;
+};
